71b0d9d9a7434ae108964f227b6f22884ff0ecf3
Fabien Ferrando <fabien@lajungle.fr>
2024-05-14 12:33:43 +0200

    teste affichage de la page d'acceuil template de la page d'acceuil template de la 404

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use(expressLayouts);
-app.set('layout', 'main');
+app.set('layout', 'layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', router);
