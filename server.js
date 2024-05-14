3e42b6e8f24859ce1d3c37e65396c7673e747f7e
Fabien Ferrando <fabien@lajungle.fr>
2024-05-14 12:17:32 +0200

    Configuration du serveur Express dans server.js mise en place de la connection avec mongodb sur un serveur atlas avec sécurisation de la clé du serveur avec dotenv mis en place tu templating ejs mise en place de la redirection vers la page 404

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-
 dotenv.config();
+import expressLayouts from'express-ejs-layouts';
+import session from 'express-session';
+
+import connectDB from './server/config/db.js';
+import router from './server/routes/router.js';
+
 const app = express();
-const port = 5003;
+const port = process.env.PORT || 5001;
+
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(expressLayouts);
+app.set('layout', 'main');
+app.set('view engine', 'ejs');
+
+app.use('/', router);
+
+app.get('*', (req, res) => {
+    res.status(404).render('404');
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
 
