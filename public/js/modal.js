acb3db0d9a8be3711dc6174a1305b1303cf4540c
Fabien Ferrando <fabien@lajungle.fr>
2024-05-15 10:16:04 +0200

    hashage du mot de passe avec bcrypt

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,14 +1,9 @@
 document.getElementById('showSignModal').addEventListener('click', function (e) {
-    console.log('sign')
     e.preventDefault()
-    console.log('sign')
     document.getElementById('signModal').style.display = 'block'
 });
-console.log('lollolo')
 document.getElementById('showLoginModal').addEventListener('click', function (e) {
     e.preventDefault()
-    console.log('sign')
-
     document.getElementById('loginModal').style.display = 'block'
 });
 
@@ -17,3 +12,19 @@ document.getElementById('closeModalLogin').addEventListener('click', function ()
 });document.getElementById('closeModalSign').addEventListener('click', function () {
     document.getElementById('signModal').style.display = 'none';
 });
+getQueryParams();
+
+function getQueryParams() {
+    const params = {};
+    window.location.search.substring(1).split("&").forEach(function(part) {
+        const [key, value] = part.split("=");
+        params[key] = decodeURIComponent(value);
+    });
+    return params;
+}
+
+const params = getQueryParams();
+if (params.showModal === 'true') {
+    document.getElementById('loginModal').style.display = 'block';
+    document.getElementById('afterSign').style.display = 'block';
+}
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,9 +1,12 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export const signUser = async (req , res ) => {
     try {
         const newUser = new User(req.body);
+        newUser.password = await bcrypt.hash(newUser.password, 10);
         await newUser.save();
+        res.redirect('/?showModal=true');
     }
     catch (err) {
         console.error(err)
